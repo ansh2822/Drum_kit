16a881efdbaabe4c4556463138238299aee9f774
Anshu Kumar <121800220+ansh2822@users.noreply.github.com>
2023-08-25 22:29:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function removetransition(e)
+{
+  if(e.propertyName !== 'transform') return;
+  // console.log(e)
+  e.target.classList.remove('playing')
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',removetransition))
+window.addEventListener('keydown',function(e){
+  const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`)
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+})
